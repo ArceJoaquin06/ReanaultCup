be630ec4e9dd08c96bcec2941a7b7fb11a30971f
Joaquin Arce <d47174994@instituto.itr.edu.ar>
2024-06-14 10:38:19 -0300

    Commit restructurador alpha

--- index.py
+++ index.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def principal():
     return render_template('Menu.html')
 
-@app.route('/students')
-def Students():
-    return render_template('Alumnos.html')
+@app.route('/sports')
+def Sports():
+    return render_template('Deportes.html')
 
 @app.route('/basket')
 def Basket():
@@ -25,6 +25,14 @@ def Football():
 @app.route('/register')
 def Register():
     return render_template('Registro.html')
+
+@app.route('/court')
+def Court():
+    return render_template('Canchas.html')
+
+@app.route('/canteen')
+def Canteen():
+    return render_template('Cantina.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.2', port=3500)
