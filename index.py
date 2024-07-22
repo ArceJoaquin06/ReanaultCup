fd413e65a513c75390ace4f006aa805f40c5f651
Joaquin Arce <d47174994@instituto.itr.edu.ar>
2024-07-22 17:01:26 -0300

    commit del vicen

--- index.py
+++ index.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    username = db.Column(db.String(80), primary_key=True)
+    password = db.Column(db.String(120), unique=True, nullable=False)
+
+class Plantilla(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
 @app.route('/')
 def principal():
@@ -39,4 +54,6 @@ def Canteen():
     return render_template('Cantina.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Crear las tablas en la base de datos
     app.run(debug=True, host='127.0.0.2', port=3500)
