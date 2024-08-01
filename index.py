39c040618483f67fa0990cceda5bd569d7001a5a
Joaquin Arce <d47174994@instituto.itr.edu.ar>
2024-08-01 11:57:18 -0300

    la primera vez que michel labura, (commit de los 3)

--- index.py
+++ index.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+"""
+# Configuracion de la base de datos
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('mysql://u4drvttmsnz01lkc:fea4CHjF009G1cs0NAqi@bnaj0hkpwqpowwit5aew-mysql.services.clever-cloud.com:3306/bnaj0hkpwqpowwit5aew') or 'postgresql+psycopg2://u4drvttmsnz01lkc:fea4CHjF009G1cs0NAqi@bnaj0hkpwqpowwit5aew-mysql.services.clever-cloud.com:3306/bnaj0hkpwqpowwit5aew'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), primary_key=True)
     password = db.Column(db.String(120), unique=True, nullable=False)
 
@@ -20,7 +24,12 @@ class Equipos(db.Model):
     partidos_empatados = db.Column(db.Integer, nullable=False)
     partidos_ganados = db.Column(db.Integer, nullable=False)
     partidos_perdidos = db.Column(db.Integer, nullable=False)
-    
+
+# Creacion de tablas antes del primer request
+@app.before_first_request
+def create_tables():
+    db.create_all()
+"""
 
 @app.route('/')
 def principal():
@@ -59,6 +68,6 @@ def Canteen():
     return render_template('Cantina.html')
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Crear las tablas en la base de datos
-    app.run(debug=True, host='127.0.0.2', port=3500)
+    #with app.app_context():
+        #db.create_all()  # Crear las tablas en la base de datos
+    app.run(debug=True, port=3500)
